8dfbb52b652eab7fcce20b1262929b121133b0d6
taivan <r0745664@student.thomasmore.be>
2021-11-18 15:11:28 +0100

    1.Gatsby website initial commit and push.

--- gatsby-config.js
+++ gatsby-config.js
@@ -6,5 +6,38 @@
 
 module.exports = {
   /* Your site config here */
-  plugins: [],
+  siteMetadata: {
+      title: 'Dickytall Gatsby Site',
+      description: 'Taivan Enkhbayar'
+
+  }
+, 
+  plugins: [
+    'gatsby-plugin-react-helmet',
+    'gatsby-plugin-sass',
+    {
+      resolve: 'gatsby-source-filesystem',
+       options: {
+        name: 'src',
+        path: `${__dirname}/src/`
+        }
+    },
+    'gatsby-plugin-sharp',
+    {
+      resolve: 'gatsby-transformer-remark',
+      options: {
+        plugins: [
+          'gatsby-remark-relative-images',
+          {
+            resolve: 'gatsby-remark-images',
+            options:{
+              maxWidth: 650,
+              linkImagesToOriginal: false,
+            }
+          }
+        ]
+      }
+    }
+
+  ]
 }
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,5 +1,18 @@
 import React from "react"
+import{Link } from "gatsby"
+import Layout from "../components/layout"
+import Head from "../components/head"
 
-export default function Home() {
-  return <div>Hello world!</div>
+const IndexPage = () => {
+  return(
+    <Layout>
+      <Head title="Home"/>
+      <h1>Hello.</h1>
+      <h2> I'm Taivan, student from Thomas More</h2>
+      <p>Need to Contact? <Link to="/contact">Contact me.</Link></p>
+    </Layout>
+  )
+
 }
+
+export default IndexPage;
